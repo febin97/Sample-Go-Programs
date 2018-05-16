9404a7f7014149f1d743b84de3588d827b279cdc
febin97 <31316987+febin97@users.noreply.github.com>
2018-05-16 15:38:30 +0530

    Add files via upload

---
+++ variables_I_O.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+import "bufio"  //for standard I/O
+import "os"
+ func main() {
+	var a= "first variable"
+	fmt.Println(a)
+
+	var b,c int = 1,2
+	fmt.Println(b,c)
+
+	f:= "short"
+	fmt.Println(f)
+
+	const n = 3e10
+	fmt.Println(n)
+	fmt.Println(int64(n))
+
+	// to input a string from user
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter Text: ")
+	text,_ := reader.ReadString(' ')  // reads the string upto that character
+	fmt.Println(text)
+
+	fmt.Println("Enter Number: ")
+    var i int
+    _, err := fmt.Scanf("%d", &i)
+    fmt.Println(i)		
+    fmt.Println(err)		
+}
